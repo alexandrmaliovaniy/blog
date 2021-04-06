7e81522ef0ccca12facbccd5b2bac09e037d5457
alexandrmaliovaniy <alexandrmaliovaniy@gmail.com>
2021-04-06 23:47:12 +0300

    user input capture

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -5,11 +5,21 @@ function AuthPage() {
 
 
     const [form, newFrom] = useState(true); // Login = true; Reg = false
+    const [field, setField] = useState({
+        email: "",
+        login: "",
+        password: "",
+        confirmPassword: "",
+    })
 
     function ChangeForm(val) {
         if (val !== form) {
             newFrom(val);
         }
+    }
+    
+    function InputField(e) {
+        setField({...field, [e.target.name]: e.target.value});
     }
 
     function Login(e) {
@@ -28,10 +38,10 @@ function AuthPage() {
                     <button className={form ? "" : "active"} onClick={()=>ChangeForm(false)}>Register</button>
                 </div>
                 <form>
-                    <input type="text" className="email" placeholder="example@mail.com"/>
-                    {form ? "" : <input type="text" className="login" placeholder="login"/>}
-                    <input type="password" className="password" placeholder="password"/>
-                    {form ? "" : <input type="password" className="confirmPassword" placeholder="confirm password"/>}
+                    <input type="text" name="email" placeholder="example@mail.com" onChange={InputField}/>
+                    {form ? "" : <input type="text" name="login" placeholder="login" onChange={InputField}/>}
+                    <input type="password" name="password" placeholder="password" onChange={InputField}/>
+                    {form ? "" : <input type="password" name="confirmPassword" placeholder="confirm password" onChange={InputField}/>}
                     <input type="submit" className="sbm" value={form ? "Login" : "Register"} onClick={form ? Login : Registration}/>
                 </form>
             </div>
