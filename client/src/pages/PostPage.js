0467c0f5fe77a10cd3ba78856de2433828ef3952
alexandrmaliovaniy <alexandrmaliovaniy@gmail.com>
2021-04-15 10:34:40 +0300

    post styles

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -1,6 +1,7 @@
 import React, {useState, useCallback, useEffect} from 'react';
 import {useParams} from 'react-router-dom';
 import {useHttp} from '../hooks/http.hook';
+import './PostPage.css';
 
 function PostPage() {
     const {request} = useHttp();
@@ -8,19 +9,23 @@ function PostPage() {
         title: "",
         description: "",
         img: "",
-        content: ""
+        content: "",
+        authorLogin: "",
+        publishDate: ""
+
     })
     const articleId = useParams().id;
 
     const getData = useCallback(async() => {
         try {
             const data = (await request('/api/post/get', 'POST', [articleId]))[0];
-            console.log(data);
             setData({
                 title: data.title,
                 img: data.titleImage,
                 description: data.description,
-                content: data.content
+                content: data.content,
+                authorLogin: data.authorLogin,
+                publishDate: String(new Date(data.publishDate))
             })
         } catch(e) {
             console.log(e);
@@ -34,12 +39,19 @@ function PostPage() {
 
 
     return (
-        <article>
-            <div className="mainInformation">
-                <h1>{data.title}</h1>
-                <p>{data.description}</p>
-                {/* <img src={data.img} alt="none" /> */}
+        <article className="PostPage">
+            <div className="vote">
+
             </div>
+            <div className="mainInformation">
+                <h1 className="title">{data.title}</h1>
+                <p className="description">{data.description}</p>
+                <div className="postInfo">
+                    <div className="author"><i>written by <a href={`/user/${data.authorLogin}`}>{data.authorLogin}</a></i></div>
+                    <div className="postTime">{data.publishDate}</div>
+                </div>
+            </div>
+            <img src={data.img} alt="none" />
             <div className="content">
                 {data.content}
             </div>
