cf7bf057cda1eccd5ac6be0c494af67cca065f96
alexandrmaliovaniy <alexandrmaliovaniy@gmail.com>
2021-04-28 22:18:31 +0300

    title fix

--- client/src/components/Article/ShortArticle/ShortArticle.js
+++ client/src/components/Article/ShortArticle/ShortArticle.js
@@ -8,7 +8,7 @@ function ShortArticle(props) {
         <div className="ShortArticle">
             <img src={props.titleImage} alt={props.title}/>
             <div className="info">
-                <h1 className="title">{props.title}</h1>
+                <h1 className="title"><a href={`/post/preview/${props._id}`}>{props.title}</a></h1>
                 <p className="description">{props.description}</p>
                 <p className="author">written by <a href={`/user/${props.authorLogin}`}>{props.authorLogin}</a></p>
                 <p className="publishDate"><FontAwesomeIcon icon={faClock} /> {(new Date(props.publishDate).toLocaleString())}</p>
